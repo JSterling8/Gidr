34e28ff26950492c789aef298df0aa62ea231bc4
Jonathan Sterling <JSterling8@gmail.com>
2014-03-12 10:39:01 +0000

    Rolled back to an older version and then organised that code into subgroups for the controller group

---
+++ Gidr/GidrDiscoverLogic.h
@@ -0,0 +1,13 @@
+//
+//  GidrDiscoverLogic.h
+//  Gidr
+//
+//  Created by Administrator on 12/03/2014.
+//  Copyright (c) 2014 Gidr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GidrDiscoverLogic : NSObject
+
+@end
