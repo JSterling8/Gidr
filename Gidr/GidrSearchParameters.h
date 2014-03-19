e73f5aa40e6c632c0dc45c214fe2eb7213a8a972
Jonathan Sterling <JSterling8@gmail.com>
2014-03-19 00:30:25 +0000

    Trying to pass a parameters object from search view to search results vie

---
+++ Gidr/GidrSearchParameters.h
@@ -0,0 +1,13 @@
+//
+//  GidrSearchParameters.h
+//  Gidr
+//
+//  Created by Administrator on 18/03/2014.
+//  Copyright (c) 2014 Gidr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GidrSearchParameters : NSObject
+
+@end
---
+++ Gidr/GidrSearchParameters.m
@@ -0,0 +1,13 @@
+//
+//  GidrSearchParameters.m
+//  Gidr
+//
+//  Created by Administrator on 18/03/2014.
+//  Copyright (c) 2014 Gidr. All rights reserved.
+//
+
+#import "GidrSearchParameters.h"
+
+@implementation GidrSearchParameters
+
+@end
